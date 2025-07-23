662ae6c3e34fcfaa73274a340e18706776be45c2
kigehiflorence <florencekigehi@gmail.com>
2025-07-23 14:00:07 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -6,9 +6,3 @@ export const useRecipeStore = create((set) => ({
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
 }));
-export const useRecipeActions = () => {
-  const addRecipe = useRecipeStore((state) => state.addRecipe);
-  const setRecipes = useRecipeStore((state) => state.setRecipes);
-  
-  return { addRecipe, setRecipes };
-};
