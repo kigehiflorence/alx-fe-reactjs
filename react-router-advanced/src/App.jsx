a174291c0ae245cd5a0348b2e080d74848185a9e
kigehiflorence <florencekigehi@gmail.com>
2025-08-20 00:12:18 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 // Pages & Components
 import Home from "./pages/Home";
-import About from "./components/About"; // About was in components
+import About from "./components/About";
+import RegistrationForm from "./components/RegistrationForm";
+import PostsComponent from "./components/PostsComponent";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
-import Profile from "./pages/Profile"; // Profile was in pages
+import BlogDetail from "./components/BlogDetail"; 
+import Profile from "./pages/Profile";
 import ProfileDetails from "./components/ProfileDetails";
 import ProfileSettings from "./components/ProfileSettings";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -18,6 +21,8 @@ function App() {
       <nav className="p-4 bg-gray-200 flex gap-4">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/posts">Posts</Link>
         <Link to="/blog">Blog</Link>
         <Link to="/profile">Profile</Link>
       </nav>
@@ -26,10 +31,13 @@ function App() {
         {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/posts" element={<PostsComponent />} />
 
         {/* Blog with dynamic routing */}
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/blog/:id/detail" element={<BlogDetail />} /> {/* extra detail route */}
 
         {/* Protected Profile Routes */}
         <Route element={<ProtectedRoute />}>
